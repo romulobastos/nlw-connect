1d85452f4cb15a1b9f637d007e69c44f350a6e3f
Rômulo Bastos <romulobastos.design@gmail.com>
2025-02-20 11:01:50 -0300

    :sparkles: feat: add fake data disclaimer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Lightbulb } from 'lucide-react'
 import './globals.css'
 
 import type { Metadata } from 'next'
@@ -27,6 +28,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${oxanium.variable} ${montserrat.variable}`}>
       <body className="bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top-center md:bg-right-top">
+        {/* disclaimer */}
+        <div className="bg-white/4 text-sm py-2 px-4 flex flex-col sm:flex-row justify-center items-center gap-4 hover:bg-white/8 transition-colors duration-300">
+          <div className="flex justify-center items-center border border-amber-300/20 min-w-10 h-10 rounded-full">
+            <Lightbulb className="text-amber-300 size-5" />
+          </div>
+          <p>
+            Os dados apresentados nesta página são fictícios e foram gerados
+            para fins de demonstração.
+          </p>
+        </div>
+
+        {/* main content */}
         <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
           {children}
         </main>
